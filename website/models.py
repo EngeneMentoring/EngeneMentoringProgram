b6b0a0c180fec454debaf8540ec24f0e6be2bc7e
EngeneMentoring <109572235+EngeneMentoring@users.noreply.github.com>
2022-08-02 10:58:53 +0530

    v02-08-22

--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     private = db.Column(db.Boolean, default=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # capsuleId=db.column(db.string(1000))
+    capsuleId=db.Column(db.String(150))
 
 
 class User(db.Model, UserMixin):
@@ -18,4 +18,6 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    school_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    # admin=db.Column(db.boolean,default=False)
